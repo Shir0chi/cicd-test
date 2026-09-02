7514c427987b98953ebf25e4f01f98b6ed347b2c
Main <m.gneushev02@yandex.ru>
2026-09-03 01:21:10 +0500

    OK - v2

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_health():
     client = app.test_client()
     response = client.get('/health')
     assert response.status_code == 200
-    assert response.data == b'OK'
+    assert response.data == b'OK - v2'
